b96bbf63ac973420829e40845220394dabf5d0fb
DONTASKME <bubblybaby069>
2026-03-04 00:27:04 +0530

    new update

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://foodhunt-website.onrender.com",
+  baseURL: "https://foodhunt-website.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
