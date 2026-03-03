e22e8e64f17a6c3f3b244e2f68befece4f91ed23
DONTASKME <bubblybaby069>
2026-03-04 00:10:08 +0530

    ...

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://food-hunt-website.vercel.app/",
+      "https://food-hunt-website.vercel.app",
     ],
     credentials: true,
   })
